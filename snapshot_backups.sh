b2d9c1b2418c6d05a9f86a8260a01b073bc02803
tcooksd <tcooksd@gmail.com>
2014-06-24 15:57:30 -0700

    Create snapshot_backups.sh

---
+++ snapshot_backups.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+#Tim Cook 
+#tim.cook@hightail.com
+
+
+
+auth="--os-username admin --os-password 731d7679ce7345eb --os-tenant-name admin --os-auth-url http://10.177.95.20:5000/v2.0/"
+export PATH=/usr/bin:/bin
+
+
+nova_chef_virt=`nova $auth list | grep kvm_chef  | awk '{print $2}' `
+nova_mrepo_virt=`nova $auth list | grep kvm_mrepo  | awk '{print $2}'`
+
+
+nova_chef_snap=`nova $auth image-list | grep kvm_chef | awk '{print $2}' `
+nova_mrepo_snap=`nova $auth image-list | grep kvm_mrepo | awk '{print $2}' `
+
+if [ $1 ] ; then
+   debug="yes"
+else
+   debug="no"
+fi
+
+create_new_image () 
+
+{
+        # if you want to see the output without running the command , for troubleshooting , 
+        # just pass any arg and the cammands that will run will be echoed to the screen without actually running. 
+        snap_name="kvm_$1_snap"
+        nova $auth image-list | grep kvm_$1 | awk '{print $4}'  | grep 02
+           if [ $? -ne "0" ]; then
+             if [ $4 == "yes" ]; then
+               echo "nova $auth image-create $2 $snap_name"02"" || exit
+               echo "nova $auth image-delete $3"
+             else
+               nova $auth image-create $2 $snap_name"02" || exit
+               nova $auth image-delete $3
+             fi
+           else
+             if [ $4 == "yes" ]; then
+               echo "nova $auth image-create $2 $snap_name"01"" || exit
+               echo "nova $auth image-delete $3"
+             else
+               nova $auth image-create $2 $snap_name"01" || exit
+               nova $auth image-delete $3
+             fi
+           fi
+}
+
+
+create_new_image chef $nova_chef_virt  $nova_chef_snap $debug
+create_new_image mrepo $nova_mrepo_virt  $nova_mrepo_snap  $debug
